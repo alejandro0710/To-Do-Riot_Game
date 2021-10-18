51ee14d5449b784e753c52a8cb2bc8b26bb40eb8
Alejandro Ramos <alejandro.ramosjarf@gmail.com>
2021-10-18 13:44:55 -0500

    Practica Todo

---
+++ script.js
@@ -0,0 +1,21 @@
+const input = document.querySelector('.input-text');
+const list = document.querySelector('.list');
+const elementOfList = document.querySelector('.element-of-list');
+const formText = document.querySelector('.form-text');
+
+function removeElement(event) {
+  console.log('Removiste un elemento');
+  event.target.remove();
+}
+
+input.addEventListener('keyup', (a) => {
+  if (a.keyCode === 13) {
+    let li = document.createElement('li');
+    li.classList.add('element-of-list');
+    li.setAttribute('onclick', 'removeElement(event)');
+    li.textContent = input.value;
+    list.appendChild(li);
+
+    formText.reset();
+  }
+});
